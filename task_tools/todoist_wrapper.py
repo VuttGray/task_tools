0be3fdbee9fc0d69f9f8e2d96d05c448d218a1bb
Stepanov, Denis <Denis.Stepanov@psi-cro.com>
2023-06-30 21:32:26 +0300

    Add json encoders to save data. Remove unused methods. Refactoring.

--- task_tools/todoist_wrapper.py
+++ task_tools/todoist_wrapper.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import json
 import re
 from dataclasses import dataclass
 from logging import getLogger
@@ -51,15 +52,12 @@ def get_todoist_link(text, url):
 
 
 class TodoistWrapper:
+
     def __init__(self):
         self.__api = TodoistAPI(conf.token)
         self.projects = self.get_projects()
         self.labels = self.get_labels()
         logger.info('Todoist wrapper created')
-
-    # def sync(self):
-    #     logger.info('Todoist sync')
-    #     self.__api.sync()
 
     def add(self, content, due, labels, project=None, priority=None) -> str:
         """
@@ -91,14 +89,6 @@ class TodoistWrapper:
             if project.name.lower() == project_name.lower():
                 return project.id
 
-    # def find_labels(self, labels: list[str]) -> []:
-    #     found_labels = []
-    #     labels = [label.lower() for label in labels]
-    #     for label in self.labels.values():
-    #         if label.name.lower() in labels:
-    #             found_labels.append(label.id)
-    #     return found_labels
-
     def search_projects(self, search: str) -> []:
         found_projects = []
         for p in self.projects.values():
@@ -121,6 +111,12 @@ class TodoistWrapper:
 
     def get_open_tasks(self) -> []:
         return [TodoistTask(i, self) for i in self.__api.get_tasks()]
+
+    def print_tasks(self):
+        tt = self.__api.get_tasks()
+        print(f'{len(tt)} tasks:')
+        for t in tt:
+            print(t)
 
     def get_closed_tasks(self, date_from: Union[dt.date, dt.datetime] = dt.date.today()) -> []:
         raise NotImplementedError
@@ -152,6 +148,13 @@ class TodoistLabel:
         return self.name
 
 
+class TodoistLabelEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, TodoistLabel):
+            return obj.__dict__
+        return json.JSONEncoder.default(self, obj)
+
+
 class TodoistProject:
     def __init__(self, project, is_archived: bool = False, is_deleted: bool = False):
         self.id = project.id
@@ -162,6 +165,13 @@ class TodoistProject:
 
     def __repr__(self):
         return self.name
+
+
+class TodoistProjectEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, TodoistProject):
+            return obj.__dict__
+        return json.JSONEncoder.default(self, obj)
 
 
 class TodoistTask:
@@ -188,7 +198,7 @@ class TodoistTask:
         content = self.parse_time_period(content)
         content = self.parse_links(content)
         self.__pure_content = content
-        self.__duration = self.get_duration()
+        self.__duration = 0
 
     @property
     def is_open(self):
@@ -200,31 +210,25 @@ class TodoistTask:
 
     def __init__(self, task: {}, tw: TodoistWrapper):
         self.id = task.id
-        self.__pure_content = task.content
 
         self.is_closed = task.is_completed
         # self.closed_date = not supported in v.2
         self.created_date = dtu_parse(task.created_at) if task.created_at else None
         self.is_recurring = task.due.is_recurring if task.due else None
         self.date = dtu_parse(task.due.date) if task.due and task.due.date else None
-        label_ids = task.labels if task.labels else []
-        self.labels = [tw.labels.get(lid) for lid in label_ids]
+        self.label_names = task.labels if task.labels else []
         self.project = tw.projects.get(task.project_id)
+        self.original_content = task.content
+        self.description = task.description
+        self.due = task.due.string if task.due else ''
 
         # parse content
         self.__pure_content = self.__duration = self.__short_duration = self.__start_time = self.__end_time = None
         self.__links = []
-        self.raw_content = task.content
+        self.raw_content = self.original_content
 
     def __repr__(self):
         return self.pure_content
-
-    def get_duration(self) -> int:
-        # cross join task labels and duration labels dictionary to get only duration labels for the task
-        duration_labels = set([lbl.id for lbl in self.labels if lbl]) & set(conf.duration_labels.keys())
-        if duration_labels:
-            return max([conf.duration_labels[lid].minutes for lid in duration_labels])
-        return 0
 
     def process_links(self, links: {}):
         content = self.raw_content
@@ -262,5 +266,29 @@ class TodoistTask:
             content = content.replace(t, f'[{t}]({l})')
         return content
 
-    def check_label(self, label):
-        return label.lower() in [lbl.name.lower() for lbl in self.labels]
+    @staticmethod
+    def get_labels_by_name(label_names: list[str], tw: TodoistWrapper) -> list[TodoistLabel]:
+        labels = []
+        for ln in label_names:
+            for key in tw.labels:
+                if tw.labels[key].name == ln:
+                    labels.append(tw.labels[key])
+                    break
+        return labels
+
+
+class TodoistTaskEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, TodoistTask):
+            data = {}
+            for key in obj.__dict__:
+                value = obj.__dict__[key]
+                if key.startswith('_'):
+                    continue
+                if isinstance(value, dt.datetime):
+                    value = value.strftime("%Y-%m-%m %H:%M:%S")
+                if isinstance(value, TodoistProject):
+                    value = value.name
+                data[key] = value
+            return data
+        return json.JSONEncoder.default(self, obj)
